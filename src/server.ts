cfe4e523160cf7296cf7eac091d17915166dd6b0
Rafael de Souza Pereira <57875932+rafaelpereiradev@users.noreply.github.com>
2023-01-20 00:43:59 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,13 @@ import { ApiError } from './helpers/api-errors';
 
 const app = express();
 app.use((req, res, next) => {
+	console.log('passou no middleware cors')
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+
+    res.header("Access-Control-Allow-Headers", "Origin, X-Request-Width, Content-Type, Accept");
     app.use(cors());
     next();
 });
