758970a0f0153eca78b3490faacec6b94b0dcc09
Rafael de Souza Pereira <57875932+rafaelpereiradev@users.noreply.github.com>
2023-01-20 00:38:12 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,14 @@ import { connectDatabase } from './database/database';
 import { ApiError } from './helpers/api-errors';
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 dotenv.config();
 const port = process.env.PORT || 3333;
 
