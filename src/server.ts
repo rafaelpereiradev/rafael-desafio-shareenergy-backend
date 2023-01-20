aab4d57fe94642ef2eaedb6937bfedb055f40670
Rafael de Souza Pereira <57875932+rafaelpereiradev@users.noreply.github.com>
2023-01-20 00:33:08 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
-import router from './routes';
 import cors from 'cors'
+import router from './routes';
 import dotenv from 'dotenv'
 import { connectDatabase } from './database/database';
 import { ApiError } from './helpers/api-errors';
 
-dotenv.config();
-const port = process.env.PORT || 3333;
 const app = express();
 app.use(cors());
+dotenv.config();
+const port = process.env.PORT || 3333;
+
 app.use(express.json());
 app.use(router);
 
